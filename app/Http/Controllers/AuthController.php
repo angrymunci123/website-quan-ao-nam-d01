2e160cddeeb1c7f31c0f4320d7a0460014177f40
Khanh_Dang <dangkhanh2747@gmail.com>
2024-06-06 21:56:45 +0700

    sua phan auth, dang nhap, dang ky admin, customer

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,30 +8,38 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function login_form()
+    {
+        if (Auth::check()) {
+            return view('admin.dashboard.dashboard');
+        }
+        return view('login');
+    }
+
     public function loginProcess(Request $request)
     {
         $credentials = $request->only('email', 'password');
-    if (Auth::attempt($credentials)) {
-        $user = Auth::user();
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->position == 'admin') {
+                $request->session()->put('user_id', $user->user_id);
+                $request->session()->put('full_name', $user->full_name);
+                $request->session()->put('position', $user->position);
+                return view('admin.dashboard.dashboard');
+            }
 
-        $request->session()->put('user_id', $user->user_id);
-        $request->session()->put('full_name', $user->full_name);
-        $request->session()->put('position', $user->position);
+            else if ($user->position == 'customer') {
+                $request->session()->put('user_id', $user->user_id);
+                $request->session()->put('full_name', $user->full_name);
+                $request->session()->put('position', $user->position);
+                return view('customer.mainpage');
+            }
 
-        if ($user->position == 'admin') {
-            return view('admin.dashboard.dashboard');
+            // else {
+            //     Auth::logout();
+            //     return redirect('login')->with('fail', 'Tài khoản này có thể chưa được cấp quyền. Vui lòng thử lại sau.');
+            // }
         }
-
-        else if ($user->position == 'customer') {
-            return view('customer.dashboard.dashboard');
-        }
-
-        else {
-            Auth::logout();
-            return redirect('login')->with('fail', 'Tài khoản này có thể chưa được cấp quyền. Vui lòng thử lại sau.');
-        }
-    }
-
     return redirect('login')->with('fail', 'Sai địa chỉ email hoặc mật khẩu. Vui lòng thử lại.');
     }
 }
