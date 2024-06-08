cdd418c88b3dd00282df28f47c04fa950dd1508c
Cong <unizzng144@gmail.com>
2024-06-09 01:41:54 +0700

    lmao

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -4,37 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+use App\Models\Product_Detail;
 
 class StoreController extends Controller
 {
-    public function mainpage(){
+    public function mainpage()
+    {
         return view("customer.index");
     }
-    public function contact(){
+    public function contact()
+    {
         return view("customer.contact");
     }
-    public function shop(){
-        return view("customer.shop");
+    public function shop()
+    {
+        $product_details = Product_Detail::with('product')->get();
+
+        return view("customer.shop", compact('product_details'));
     }
-    public function shopping_cart(){
+    public function shopping_cart()
+    {
         return view("customer.shopping-cart");
     }
-    public function product_detail(){
+    public function product_detail()
+    {
         return view("customer.product-details");
     }
-    public function checkout(){
+    public function checkout()
+    {
         return view("customer.checkout");
     }
-    public function blog(){
+    public function blog()
+    {
         return view("customer.blog");
     }
-    public function blog_detail(){
+    public function blog_detail()
+    {
         return view("customer.blog-details");
     }
-    public function about(){
+    public function about()
+    {
         return view("customer.about");
     }
-    public function login_customer(){
+    public function login_customer()
+    {
         return view("customer.login_cus");
     }
 }
