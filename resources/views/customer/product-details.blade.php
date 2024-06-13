b5b6446eb1284feebd7b156a7ccc74e9ed3f553b
Cold Palmer <zipzip3579@gmail.com>
2024-06-13 20:00:17 +0700

    them sp, sua trang detail cus

--- resources/views/customer/product-details.blade.php
+++ resources/views/customer/product-details.blade.php
@@ -89,34 +89,30 @@
                                         <input type="hidden" name="product_detail_id" value="{{$product_detail->product_detail_id}}"/>
                                         <input type="hidden" name="product_id" value="{{$product_detail->product_id}}"/>
                                         
-                                        <table>
-                                            <tr>
-                                                <th>
-                                                    <label>Kích Cỡ</label>
+                                            <div class="detail_container">
+                                                <div>
+                                                    <label for="size"><b>Kích Cỡ</b></label>
                                                     <br>
-                                                    <select name="size" class="form-control" style="width: 150px;">
+                                                    <select name="size" id="size" class="form-control">
                                                         @foreach($product_size as $display_size)
                                                             <option value="{{$display_size->size}}">{{$display_size->size}}</option>
                                                         @endforeach
                                                     </select>
-                                                </th>
-                                                <th>
-                                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                </th>
-                                                <th>
-                                                    <label>Màu Sắc</label>
-                                                    <br>
+                                                </div>
+                                                <div></div>
+                                                <br>
+                                                <div>
+                                                    <label><b>Màu Sắc: </b></label>
                                                     @foreach($product_colors as $color)
                                                         <label for="color-{{ $color->index }}">{{ $color->color }}
                                                             <input type="radio" id="color-{{ $color->index }}" name="color" value="{{ $color->color }}">
                                                         </label>
                                                     @endforeach
-                                                </th>
-                                            </tr>
-                                        </table>
+                                                </div>
+                                            </div>
                                         <br>
                                         <div class="quantity">
-                                            <label>Số Lượng:</label>
+                                            <label><b>Số Lượng:</b></label>
                                             <div class="product__details__cart__option">
                                                 <div class="quantity">
                                                     <div class="pro-qty">
