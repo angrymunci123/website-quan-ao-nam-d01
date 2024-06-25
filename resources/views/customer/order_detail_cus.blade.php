a287992befd6b10395aae7f0708ad463c21aec5f
Thanh99999 <zipzip3579@gmail.com>
2024-06-25 13:48:58 +0700

    them popup huy don hang

--- resources/views/customer/order_detail_cus.blade.php
+++ resources/views/customer/order_detail_cus.blade.php
@@ -2,41 +2,43 @@
 @section('content')
 <!-- Breadcrumb Section Begin -->
 <section class="breadcrumb-option">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="breadcrumb__text">
-                        <h4 style="color: white">Chi tiết đơn hàng</h4>
-                        <div class="breadcrumb__links">
-                            <a href="./index.html" style="color: white">Trang chủ</a>
-                            <span>Chi tiết đơn hàng</span>
-                        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb__text">
+                    <h4 style="color: white">Chi tiết đơn hàng</h4>
+                    <div class="breadcrumb__links">
+                        <a href="./index.html" style="color: white">Trang chủ</a>
+                        <span>Chi tiết đơn hàng</span>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- Breadcrumb Section End -->
-     <br>
+    </div>
+</section>
+<!-- Breadcrumb Section End -->
+<br>
 <div class="container">
     <div class="card-body">
-            @if(Session::has('notification'))
-                <div class="alert alert-success" style="color:white">
-                    {{Session::get('notification')}}
-                </div>
-            @endif
+        @if(Session::has('notification'))
+            <div class="alert alert-success" style="color:white">
+                {{Session::get('notification')}}
+            </div>
+        @endif
     </div>
-<div class="checkout__order">
-    @php
-        $total_price_order = 0;
-    @endphp
-    <h4 class="order__title">Đơn hàng</h4>
+    <div class="checkout__order">
+        @php
+            $total_price_order = 0;
+        @endphp
+        <h4 class="order__title">Đơn hàng</h4>
         <div>
-        @foreach($order_details as $order_detail)  @endforeach
-            <table class="table align-items-center mb-0" style="background-color: #F3F2EE; padding-left: 15px; padding-right: 15px; border-style: none">
-                 <tbody>
+            @foreach($order_details as $order_detail)  @endforeach
+            <table class="table align-items-center mb-0"
+                style="background-color: #F3F2EE; padding-left: 15px; padding-right: 15px; border-style: none">
+                <tbody>
                     <tr style="width:100%">
-                        <td class="font-weight-bolder opacity-7" style="background-color: #F3F2EE; float: left; width: 50%">
+                        <td class="font-weight-bolder opacity-7"
+                            style="background-color: #F3F2EE; float: left; width: 50%">
                             <div>
                                 <p><b>Họ tên người nhận: {{$order_detail->consignee}}</b></p>
                                 <p><b>SDT: {{$order_detail->phone_number}}</b></p>
@@ -48,12 +50,12 @@
                             <div style="float:right">
                                 <p><b>ID đơn hàng: {{$order_detail->order_id}}</b></p>
                                 <p><b>Tình trạng đơn hàng: {{$order_detail->status}}</b></p>
-                                 <p><b>Phương thức thanh toán: {{$order_detail->payment_method}}</b></p>
+                                <p><b>Phương thức thanh toán: {{$order_detail->payment_method}}</b></p>
                             </div>
                         </td>
                     </tr>
                 </tbody>
-             </table>
+            </table>
         </div>
         <table class="table">
             <tr>
@@ -63,17 +65,18 @@
                 <th scope="col">Thành tiền</th>
             </tr>
             @foreach($product_order as $product)
-                @php
-                $total = 0;
-                $total += $product->price * $product->quantity;
-                $total_price_order += $total;
-                @endphp
-            <tr>
-                <td><a href="/ktcstore/product/{{$product->product_name}}" style="color:black">{{$product->product_name}}</a></td>
-                <td>{{$product->quantity}}</td>
-                <td>{{number_format($product->price)}}đ</td>
-                <td>{{number_format($total)}}đ</td>
-                </tr>
+                        @php
+                            $total = 0;
+                            $total += $product->price * $product->quantity;
+                            $total_price_order += $total;
+                        @endphp
+                        <tr>
+                            <td><a href="/ktcstore/product/{{$product->product_name}}"
+                                    style="color:black">{{$product->product_name}}</a></td>
+                            <td>{{$product->quantity}}</td>
+                            <td>{{number_format($product->price)}}đ</td>
+                            <td>{{number_format($total)}}đ</td>
+                        </tr>
             @endforeach
         </table>
         <ul class="checkout__total__all">
@@ -93,17 +96,43 @@
                     </form>
                 </td> -->
                 @if ($order_detail->status == 'Đang chờ xác nhận')
-                <td>
-                    <form action="/ktcstore/order_history/cancel_order/order_id={{ $order_detail->order_id }}" method="POST"
-                     style="padding-right: 20px; float: right">
-                     @csrf
-                        <button type="submit" class="btn btn-danger" style="width: 150px; color:white" onclick="return confirm('Bạn chắc chắn muốn hủy đơn hàng này không? Điều này không thể hoàn tác lại')">Hủy Đơn Hàng</button>
-                    </form>
-                </td>
+                    <td>
+                        <form action="/ktcstore/order_history/cancel_order/order_id={{ $order_detail->order_id }}"
+                            method="POST" style="padding-right: 20px; float: right">
+                            @csrf
+                            <button id="cancelOrderForm" type="button" class="btn btn-danger"
+                                style="width: 150px; color:white" onclick="showCancelOrderModal()">Hủy Đơn Hàng</button>
+                        </form>
+                    </td>
                 @endif
+                <div class="modal fade" id="cancelOrderModal" tabindex="-1" role="dialog"
+                    aria-labelledby="cancelOrderModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="cancelOrderModalLabel">Xác nhận hủy đơn hàng</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Bạn chắc chắn muốn hủy đơn hàng này không? Điều này không thể hoàn tác lại.
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+                                <form action="/ktcstore/order_history" method="GET"
+                                    style="padding-right: 20px; float: right">
+                                    <button type="button" class="btn btn-danger" id="confirmCancelOrder">Hủy Đơn
+                                        Hàng</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <td>
                     <form action="/ktcstore/order_history" method="GET" style="padding-right: 20px; float: right">
-                        <button type="submit" class="btn btn-success" style="width: 110px; color:white">Quay lại</button>
+                        <button type="submit" class="btn btn-success" style="width: 110px; color:white">Quay
+                            lại</button>
                     </form>
                 </td>
             </tr>
@@ -128,4 +157,14 @@
       });
     }
   </script> -->
+<script>
+
+    function showCancelOrderModal() {
+        $('#cancelOrderModal').modal('show');
+    }
+
+    document.getElementById('confirmCancelOrder').addEventListener('click', function () {
+        document.getElementById('cancelOrderForm').submit();
+    });
+</script>
 @endsection
