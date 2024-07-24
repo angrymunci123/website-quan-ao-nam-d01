6da3525ed0b9a0c9802948e2859105a72ab37f31
Thanh99999 <zipzip3579@gmail.com>
2024-07-25 00:02:00 +0700

    mukbang

--- resources/views/customer/contact.blade.php
+++ resources/views/customer/contact.blade.php
@@ -1,5 +1,21 @@
 @extends('customer.layout')
 @section('content')
+
+<section class="breadcrumb-option">
+        <div class="container" style="background-image: url('temp_assets/img/banner.png')">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb__text">
+                        <h4 style="color: white">Cửa hàng</h4>
+                        <div class="breadcrumb__links">
+                            <a href="/ktcstore" style="color: white">Trang chủ</a>
+                            <span style="color: white">Liên hệ</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <!-- Contact Section Begin -->
     <section class="contact spad">
         <div class="container">
@@ -9,16 +25,20 @@
                         <div class="section-title">
                             <span>Thông tin</span>
                             <h2>Liên hệ với chúng tôi</h2>
-                            <p>Bạn có gọi điện thì chúng tôi ko nghe máy, bạn có nhắn tin thì chúng tôi seen ko rep</p>
+                            <p>Chào mừng bạn đến với trang liên hệ của chúng tôi! Chúng tôi luôn sẵn lòng lắng nghe và hỗ trợ bạn. <br> Nếu bạn có bất kỳ câu hỏi nào về sản phẩm, dịch vụ hoặc cần hỗ trợ mua hàng, đừng ngần ngại liên hệ với chúng tôi qua các thông tin dưới đây.</p>
                         </div>
                         <ul>
                             <li>
-                                <h4>Nhân viên chăm sóc khách hàng A</h4>
-                                <p>0384758374</p>
+                                <h4>Thông Tin Liên Hệ</h4>
+                                <p>Địa chỉ: 124 Giải Phòng, Quận Hai Bà Trưng, Hà Nội</p>
+                                <p>Hotline: 0912345678</p>
+                                <p>Email: ktcstoreservice@gmail.com</p>
                             </li>
                             <li>
-                                <h4>Nhân viên chăm sóc khách hàng B</h4>
-                                <p>0283745625</p>
+                                <h4>Giờ Làm Việc</h4>
+                                <p>Thứ Hai - Thứ Sáu: 9:00 - 20:00</p>
+                                <p>Thứ Bảy: 10:00 - 18:00</p>
+                                <p>Chủ Nhật: 11:00 - 17:00</p>
                             </li>
                         </ul>
                     </div>
